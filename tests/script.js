8e4d0ee7edb5bc69f42db78149e106ed8a8f7cab
lukas <sl3mey@icloud.com>
2024-02-06 21:44:01 +0100

    Initial commit

---
+++ tests/script.js
@@ -0,0 +1,9 @@
+var x = prompt("What do you want to play? (TicTacToe, JStacking) please enter the names exactly like that")
+
+if(x=="TicTacToe"){
+    window.location.href = "tictactoe/index.html"
+}
+if(x == "JStacking"){
+    window.location.href = "JStacking/index.html"
+}
+
